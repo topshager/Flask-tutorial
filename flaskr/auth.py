1c5bbacd45ac97b4e2d92cf1defdc3ce74a20c0a
topshager <rem28777@gmail.com>
2024-07-12 00:42:33 +0200

    Update auth.py

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -67,3 +67,18 @@ def login():
             return redirect(url_for('index'))
         flash(error)
     return render_template('auth/login.html')
+
+
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
+
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+        return view(**kwargs)
+    return wrapped_view
