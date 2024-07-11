b64a4c71dbfd70b5086fcef4d051507f898ce32a
topshager <rem28777@gmail.com>
2024-07-12 00:29:31 +0200

    Update auth.py

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -36,6 +36,18 @@ def register():
     return render_template('auth/register.html')
 
 @bp.route('/login',methods=('GET', 'POST'))
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id == ?',(user_id,)
+        ).fetchone()
+
 def login():
     if request.method == 'POST':
         username = request.form['username']
