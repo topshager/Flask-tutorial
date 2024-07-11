133342dcde98ffd6a7cbb941b9fc3aa863b202f8
topshager <rem28777@gmail.com>
2024-07-11 20:48:44 +0200

    Update __init__.py

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,13 +1,18 @@
 import os
 from flask  import Flask
+from.import db
+from . import auth 
 
 def create_app(test_config=None):
     app = Flask(__name__,instance_relative_config=True)
     #creating a flask instance
+
+
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
+
     if test_config is None:
         #load the instance config, if it exists ,when not testing
         app.config.from_pyfile('config.py',silent=True)
@@ -18,6 +23,9 @@ def create_app(test_config=None):
         os.makedirs(app.instance_path)
     except OSError:
         pass
+
+    db.init_app(app)
+    app.register_blueprint(auth.bp)
     #a simple page that says hello
     @app.route('/hello')
     def hello():
