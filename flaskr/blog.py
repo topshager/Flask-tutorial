670d6e74c4d252329020ff5d09fea74728eaf39a
topshager <rem28777@gmail.com>
2024-07-12 23:39:58 +0200

    Update blog.py

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -58,4 +58,32 @@ def get_post(id,check_author=True):
 @bp.rout('/<int:id>/update',mehtods=('GET','POST'))
 @login_required
 def update(id):
-    
+    post =  get_post(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error == None
+        if not title:
+            error =' Title is required'
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'UPDATE post set title = ?,body=?'
+                'WHERE id = ?'
+                (title,body,id)
+            )
+            db.commit()
+            return redirect(url_for('blog.index'))
+    return render_template('blog/update.html',post=post)
+
+@bp.route('/<int:id>?delet',methods=('POST',))
+@login_required
+def delete(id):
+    get_post(id)
+    db = get_db
+    db.execute('DELETE FROM post WHERE id=?',(id))
+    db.commit()
+    return redirect(url_for('blog.index'))
