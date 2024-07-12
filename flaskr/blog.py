8c0d9f5372325f614aec417ef6380bd7d3bc385c
topshager <rem28777@gmail.com>
2024-07-12 22:09:53 +0200

    Update blog.py

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -9,3 +9,12 @@ from flaskr.db import get_db
 
 bp = Blueprint('blog',__name__)
 
+@bp.route('/')
+def index():
+    db = get_db()
+    posts = db.execute(
+        'SELECT p.id, title,body,created,author_id,username'
+        'FROM post p JOIN user u ON p.author_id = u.id'
+        'ORDER BY CREATED DESC'
+    ).fetchall()
+    return render_template('blog/index.html',posts=posts)
