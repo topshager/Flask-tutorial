87e1807763a027f670e688ca563f447f2bbec9aa
topshager <rem28777@gmail.com>
2024-07-13 01:00:54 +0200

    Update blog.py

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -2,7 +2,7 @@ from flask import(
     Blueprint,flash,g,redirect,render_template,request,url_for
 )
 from werkzeug.exceptions import abort
-from flaskr.auth import abort
+from flask import abort
 from flaskr.auth import login_required
 from flaskr.db import get_db
 
@@ -13,10 +13,10 @@ bp = Blueprint('blog',__name__)
 def index():
     db = get_db()
     posts = db.execute(
-        'SELECT p.id, title,body,created,author_id,username'
-        'FROM post p JOIN user u ON p.author_id = u.id'
+        'SELECT p.id, title, body, created, author_id, username '
+        'FROM post p JOIN user u ON p.author_id = u.id '
         'ORDER BY CREATED DESC'
-    ).fetchall()
+    ).fetchone()
     return render_template('blog/index.html',posts=posts)
 
 @bp.route('/create',methods=('GET','POST'))
@@ -35,12 +35,12 @@ def create():
         else:
             db = get_db()
             db.execute(
-                'INSERT INTO post (title,body,author_id)'
-                'VALUES(?,?,?)'
+                'INSERT INTO post (title, body, author_id)'
+                'VALUES(?, ?, ?)',
                 (title,body,g.user['id'])
             )
             db.commit()
-            return redirect('blog/create.html')
+            return redirect(url_for('blog/create.html'))
 
 def get_post(id,check_author=True):
     post = get_db().execute(
@@ -55,7 +55,7 @@ def get_post(id,check_author=True):
         abort(403)
     return post
 
-@bp.rout('/<int:id>/update',mehtods=('GET','POST'))
+@bp.route('/<int:id>/update',methods=('GET','POST'))
 @login_required
 def update(id):
     post =  get_post(id)
@@ -63,7 +63,7 @@ def update(id):
     if request.method == 'POST':
         title = request.form['title']
         body = request.form['body']
-        error == None
+        error = None
         if not title:
             error =' Title is required'
         if error is not None:
@@ -71,8 +71,8 @@ def update(id):
         else:
             db = get_db()
             db.execute(
-                'UPDATE post set title = ?,body=?'
-                'WHERE id = ?'
+                'UPDATE post SET title = ?,body=?'
+                'WHERE id = ?',
                 (title,body,id)
             )
             db.commit()
@@ -83,7 +83,7 @@ def update(id):
 @login_required
 def delete(id):
     get_post(id)
-    db = get_db
-    db.execute('DELETE FROM post WHERE id=?',(id))
+    db = get_db()
+    db.execute('DELETE FROM post WHERE id= ?',(id))
     db.commit()
     return redirect(url_for('blog.index'))
